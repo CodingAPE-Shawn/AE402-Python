4f061075206bc0461f3dc378218607ba538b973f
Shawn <58027264+CodingAPE-Shawn@users.noreply.github.com>
2020-10-21 09:06:56 +0800

    第六堂 課堂實作 碰撞遊戲(1對1，撞擊後分數會增加)

--- collide.py
+++ collide.py
@@ -1,3 +1,6 @@
+"""
+碰撞 -- 一對一
+"""
 import pygame
 import random
 
@@ -16,14 +19,17 @@ player_x = 0
 player_y = 0
 player_w = 50
 player_h = 50
-
 # block的初始位置
 block_w = 50
 block_h = 50
 block_x = random.randrange(screen_width-block_w)
 block_y = random.randrange(screen_height-block_h)
-
+# 判斷有無撞到的布林變數
 collision = False
+# 分數
+score = 0
+# font物件
+font = pygame.font.Font(None, 50)
 
 done = False
 
@@ -33,28 +39,33 @@ while not done:
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT: 
             done = True 
-            
     # 判斷是否碰撞到了
     xin = block_x<=player_x<=block_x+block_w or block_x<=player_x+player_w<=block_x+block_w
     yin = block_y<=player_y<=block_y+block_h or block_y<=player_y+player_h<=block_y+block_h
     if  xin and yin and not collision:
         collision = True
+        score += 1
         
+    # 清除視窗畫面
     screen.fill(WHITE)
 
-    # 取得滑鼠座標
+    # 取得滑鼠座標 (list of (x,y))
     pos = pygame.mouse.get_pos()
-    
-    # 畫出player
+    # 畫出player & block
     player_x = pos[0]
     player_y = pos[1]
-    pygame.draw.rect(screen,RED,[player_x,player_y,player_w,player_h])
-    
-    # 如果沒有碰撞劃出block方塊
+    pygame.draw.rect(screen, RED, [player_x, player_y, player_w, player_h])
     if not collision:
-        pygame.draw.rect(screen,BLACK,[block_x,block_y,block_w,block_h])
+        pygame.draw.rect(screen, BLACK, [block_x, block_y, block_w, block_h])
 
+    # 顯示分數
+    message = str(score)+' point'
+    text = font.render(message, 10, BLACK)
+    screen.blit(text, (10,10))
+    
+    # 新畫面更新
     pygame.display.flip()
-    clock.tick(60)
 
+    # 一秒60個frame
+    clock.tick(60)
 pygame.quit()
