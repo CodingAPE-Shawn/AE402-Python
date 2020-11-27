9fb380b2bdecf784bc3ed5b26846f70fb687cd19
Shawn <58027264+CodingAPE-Shawn@users.noreply.github.com>
2020-11-27 21:38:17 +0800

    1127 實作

--- one_collide.py
+++ one_collide.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+
 white = (255,255,255)
 black = (0,0,0)
 red = (255,0,0)
@@ -6,33 +8,40 @@ red = (255,0,0)
 pygame.init()
 window_size = (800,600)
 screen = pygame.display.set_mode(window_size)
+pygame.display.set_caption('collide game')
 
-#block 被撞擊的方塊
-block_x = 100
-block_y = 100
+done = False
+clock = pygame.time.Clock()
+
+block_x = random.randrange(0,800)
+block_y = random.randrange(0,600)
 block_width = 50
 block_height = 50
 
-#player
-play_x = 500
-play_y  = 500
+player_x = 300
+player_y = 300
 player_width = 50
 player_height = 50
-
-done = False
-
-clock = pygame.time.Clock()
+collide = False
 
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-    #play_x,play_y = pygame.mouse.get_pos()
+    #判斷碰撞
+    x_collide = block_x<player_x<block_x+block_width or block_x<player_x+player_width<block_x+block_width
+    y_collide = block_y<player_y<block_y+block_height or block_y<player_y+player_height<block_y+block_height
+    if x_collide and y_collide and not collide:
+        collide = True
+    
+    player_x,player_y = pygame.mouse.get_pos()
     screen.fill(white)
-    pygame.draw.rect(screen,black,[100,100,50,50])
-    pygame.draw.rect(screen,red,[play_x,play_y,50,50])
+    pygame.draw.rect(screen, red, [player_x,player_y,player_width,player_height])
+    if not collide:
+        pygame.draw.rect(screen, black, [block_x,block_y,block_width,block_height])
+    
+    
     pygame.display.flip()
     clock.tick(40)
 
 pygame.quit()
-
